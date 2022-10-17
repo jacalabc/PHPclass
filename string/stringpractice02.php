3acc2926bf04ca6209a5e2c66b800a32dc2f7269
jerry <jerry19971228@gmail.com>
2022-10-17 11:12:51 +0800

    新增陣列資料夾

--- string/stringpractice02.php
+++ string/stringpractice02.php
@@ -16,5 +16,18 @@ echo "<hr>";
 echo "字串取用"."<br>";
 $str="The reason why a great man is great is that he resolves to be a great man";
 echo mb_substr($str,0,10)."...";
-
 ?>
+
+<h1>字串函式整合應用</h1>
+<?php
+$target='程式設計';
+$str="學會PHP網頁程式設計，薪水會加倍，工作會好找";
+$sub="<span style='font-size: 24px;color:red;'>".$target."</span>";
+$result=str_replace($target,$sub,$str);
+echo $str."<br>";
+echo $result;
+echo "<hr>";
+echo join($sub,explode($target,$str))
+?>
+ 
+ <!-- 學會PHP<span style="font-size: 24px;color:red;">網頁程式</span>設計，薪水會加倍，工作會好找 -->
