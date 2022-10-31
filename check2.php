70cecef0f7ffcecda9f3298b3a1d261e5bc2708f
mackliu <mackliu@wda.gov.tw>
2022-10-31 15:10:06 +0800

    多個使用者登入

--- check2.php
+++ check2.php
@@ -1,13 +1,36 @@
 <?php
 session_start();
 
-$acc='mack';
-$pw='1234';
+$users=[
+    [
+        "name"=>"mack",
+        "pw"=>"1234",
+        "level"=>"admin",
+    ],
+    [
+        "name"=>"john",
+        "pw"=>"5678",
+        "level"=>"user",
+    ],
+    [
+        "name"=>"mary",
+        "pw"=>"1357",
+        "level"=>"vip",
+    ]];
+
+
 
 $formAcc=$_POST['acc'];
 $formPw=$_POST['pw'];
 
-if($acc==$formAcc && $pw==$formPw){
+$chk=false;
+foreach($users as $user){
+    if($user['name']==$formAcc && $user['pw']==$formPw){
+        $chk=true;
+    }
+}
+
+if($chk){
     $_SESSION['login']=$formAcc;
 }else{
     $_SESSION['error']="帳號或密碼錯誤";
